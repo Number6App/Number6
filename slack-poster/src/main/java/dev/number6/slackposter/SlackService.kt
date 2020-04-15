efa287bab43ea55ec176dd76dad0b6bd336fca71
Ian Esling <ian.esling@gmail.com>
2020-04-15 14:56:50 +0100

    KOTLIN

---
+++ slack-poster/src/main/java/dev/number6/slackposter/SlackService.kt
@@ -0,0 +1,16 @@
+package dev.number6.slackposter
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+import dev.number6.slackposter.model.PresentableChannelSummary
+import dev.number6.slackposter.port.SlackPort
+import javax.inject.Inject
+
+class SlackService @Inject constructor(private val slackPort: SlackPort) {
+    fun handleNewImage(image: ChannelSummaryImage, logger: LambdaLogger) {
+        logger.log("handling image: $image")
+        if (image.hasFinalUpdate()) {
+            slackPort.postMessageToChannel(PresentableChannelSummary(image), logger)
+        }
+    }
+
+}
