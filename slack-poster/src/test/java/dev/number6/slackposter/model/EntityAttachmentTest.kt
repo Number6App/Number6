014ec2a5851ac541ab3488825e3f9b9222934ea8
Ian Esling <ian.esling@gmail.com>
2020-04-16 17:53:02 +0100

    start assertk-ing

--- slack-poster/src/test/java/dev/number6/slackposter/model/EntityAttachmentTest.kt
+++ slack-poster/src/test/java/dev/number6/slackposter/model/EntityAttachmentTest.kt
@@ -1,37 +1,31 @@
 package dev.number6.slackposter.model
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import dev.number6.slackposter.ChannelSummaryImage
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import java.util.*
-import java.util.stream.Stream
 
 internal class EntityAttachmentTest {
     @Test
     fun createsFieldsWithExpectedValues() {
-        val expectedFieldValues: MutableMap<String?, String> = HashMap()
-        expectedFieldValues["Entity1:"] = "Entity1Object1 (3)"
-        expectedFieldValues["Entity2:"] = "Entity2Object2 (4), Entity2Object1 (2)"
-        expectedFieldValues["Entity3:"] = "Entity3Object2 (10), Entity3Object3 (2), Entity3Object1 (1)"
-        val entityTotals: MutableMap<String, AttributeValue?> = HashMap()
-        val entity1: MutableMap<String, AttributeValue> = HashMap()
-        val entity2: MutableMap<String, AttributeValue> = HashMap()
-        val entity3: MutableMap<String, AttributeValue> = HashMap()
-        entity1["Entity1Object1"] = AttributeValue().withN("3")
-        entity2["Entity2Object1"] = AttributeValue().withN("2")
-        entity2["Entity2Object2"] = AttributeValue().withN("4")
-        entity3["Entity3Object1"] = AttributeValue().withN("1")
-        entity3["Entity3Object2"] = AttributeValue().withN("10")
-        entity3["Entity3Object3"] = AttributeValue().withN("2")
-        entityTotals["Entity1"] = AttributeValue().withM(entity1)
-        entityTotals["Entity2"] = AttributeValue().withM(entity2)
-        entityTotals["Entity3"] = AttributeValue().withM(entity3)
-        val image: ChannelSummaryImage = ChannelSummaryImageBuilder.Companion.builder()
+
+        val expectedFieldValues = mapOf("Entity1:" to "Entity1Object1 (3)",
+                "Entity2:" to "Entity2Object2 (4), Entity2Object1 (2)",
+                "Entity3:" to "Entity3Object2 (10), Entity3Object3 (2), Entity3Object1 (1)")
+
+        val entityTotals = mapOf("Entity1" to AttributeValue().withM(mapOf("Entity1Object1" to AttributeValue().withN("3"))),
+                "Entity2" to AttributeValue().withM(mapOf("Entity2Object1" to AttributeValue().withN("2"),
+                        "Entity2Object2" to AttributeValue().withN("4"))),
+                "Entity3" to AttributeValue().withM(mapOf("Entity3Object1" to AttributeValue().withN("1"),
+                        "Entity3Object2" to AttributeValue().withN("10"),
+                        "Entity3Object3" to AttributeValue().withN("2"))))
+
+        val image: ChannelSummaryImage = ChannelSummaryImageBuilder.builder()
                 .entityTotals(entityTotals)
                 .build()
         val testee = EntityAttachment.fromImage(image)
-        println(Arrays.toString(testee.fields))
-        Stream.of(*testee.fields).forEach { f: Field -> Assertions.assertThat(f.value).isEqualTo(expectedFieldValues[f.title]) }
+
+        testee.fields.forEach { f -> assertThat(f.value).isEqualTo(expectedFieldValues[f.title]) }
     }
 }
